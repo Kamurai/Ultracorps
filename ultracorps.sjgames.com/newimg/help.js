e1c2b0f45cc124ddd3af1c9814b42c19a890642e
Kamurai <16156165+Kamurai@users.noreply.github.com>
2020-04-08 19:29:38 -0400

    HTTrack Update

---
+++ ultracorps.sjgames.com/newimg/help.js
@@ -0,0 +1,11 @@
+function startInfo(theURL) {
+    SpawnWindow(theURL, 'Info');
+}
+
+function SpawnWindow(theURL,WindowName,width,height) { 
+    if (!width) { width = 650; }
+    if (!height) { height = 650; }
+    infoWin = window.open(theURL,WindowName,'toolbar=0,location=0,directories=0,status=1,menubar=0,scrollbars=1,resizable=1,width=' + width + ',height=' + height);
+    infoWin.focus();
+    return false;
+}
